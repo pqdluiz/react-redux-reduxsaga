53ab8f57ed2039eab55e706eab79075d89daa468
luizaoformigapqd <luizlima@garantti.com.br>
2022-03-25 18:09:57 -0300

    style: 💄 07 - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 
 import RepositoryList from "./containers/repository-list";
 import store from "./store";
+import "./styles/global.scss";
 
 export const App: React.FC = () => {
   return (
